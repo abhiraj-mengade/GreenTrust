dc1192671848e4b011155f7e4b620af77babbd96
kid-116 <75692643+kid-116@users.noreply.github.com>
2023-02-19 18:14:31 +0000

    Deploying to gh-pages from @ abhiraj-mengade/GreenTrust@629eb531d1ade811a024eb6b67fffedb3c4891f6 🚀

--- greentrust/src/components/StatusCard.js
+++
@@ -1,7 +0,0 @@
-export default function StatusCard({ text, color }) {
-    return (
-        <div className={`text-white top-2 right-2 p-1 rounded-full px-2 text-sm w-fit ${color}`} title={text}>
-            {text}
-        </div>
-    )
-}
--- iot/config.py
+++
@@ -1,4 +0,0 @@
-IPFS_HOST = 'ipfs.infura.io'
-IPFS_FILE_HOST = 'ipfs.io/ipfs'
-
-SENSOR_ID = 1
